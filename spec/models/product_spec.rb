c35c67745c12679577d3fcc98ff6e12d65583fba
PING NGE <ping.nge@hotmail.com>
2020-09-03 01:00:10 +1000

    TESTING: refactored tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,31 +1,30 @@
-require_relative '../../lib/models/Product'
+require_relative "../../lib/models/product"
+require 'spec_helper'
 
-describe 'Product' do
-  before(:each) do
-    @product = Product.new(123, 'Test Product', 2000)
-  end
+RSpec.describe Product do
+  let (:product) { Product.new(123, "Test Product", 2000)}
 
   # Should have valid attributes
   it 'should be valid with a uuid' do
-    expect(@product.uuid).to eq 123
+    expect(product.uuid).to eq 123
   end
 
   it 'should be valid with a name' do
-    expect(@product.name).to eq 'Test Product'
+    expect(product.name).to eq 'Test Product'
   end
 
   it 'should be valid with a price' do
-    expect(@product.price).to eq 2000
+    expect(product.price).to eq 2000
   end
 
   # Should be able to update product attributes
-  it 'should be able to change product name' do
-    @product.update_name('New Name')
-    expect(@product.name).to eq 'New Name'
+  it 'should be able to update name' do
+    product.update_name('New Name')
+    expect(product.name).to eq 'New Name'
   end
 
-  it 'should be able to change product price' do
-    @product.update_price(3000)
-    expect(@product.price).to eq 3000
+  it 'should be able to update price' do
+    product.update_price(3000)
+    expect(product.price).to eq 3000
   end
 end
