c25aa0bcc0145cb28b98eba67d2e67602d7528e4
PING NGE <ping.nge@hotmail.com>
2020-09-02 21:43:16 +1000

    UPDATE: refactor menu class for readability

--- lib/view/Menu.rb
+++ lib/view/Menu.rb
@@ -1,39 +1,52 @@
 require 'tty-prompt'
 require 'json'
+require 'artii'
+require 'colorize'
 
 require_relative '../controllers/products'
 require_relative '../controllers/carts'
 
 class Menu
-  def self.run
+  # Run the application
+  def display_menu
+    a = Artii::Base.new font: 'slant'
+
     system('clear')
-    puts 'Welcome to Best Bikes!'
+    puts a.asciify('Best Bikes').colorize(:light_green)
+    puts 'Welcome to Best Bikes!'.colorize(:green)
     loop do
       prompt = TTY::Prompt.new
 
-      user_selection = prompt.select('Please select from the following options', active_color: :cyan) do |menu|
-        #Menu choices
+      user_selection = prompt.select('Please select from the following options', active_color: :green) do |menu|
+        # Menu choices
         menu.choice 'View Products', 1
         menu.choice 'View Shopping Cart', 2
         menu.choice 'Check out', 3
         menu.choice 'Exit Application', 4
       end
 
-      case user_selection
-      when 1
-        system('clear')
-        Products::view_all_products
-      when 2
-        system('clear')
-        Carts::view_cart
-      when 3
-        system('clear')
-        Carts::checkout
-      when 4
-        system('clear')
-        puts 'Thanks for visiting Best Bikes 👋👋'
-        exit
-        end
+      menu_selection(user_selection)
+    end
+  end
+
+  # Main Menu
+  def menu_selection(user_selection)
+    case user_selection
+    when 1
+      system('clear')
+      Products.view_all_products
+    when 2
+      system('clear')
+      Carts.view_cart
+    when 3
+      system('clear')
+      Carts.checkout
+    when 4
+      system('clear')
+      puts ' '
+      puts 'Thanks for visiting Best Bikes 👋👋'.colorize(:green)
+      puts ' '
+      exit
     end
   end
 end
